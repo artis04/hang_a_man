4eb272a43351390ddbac327eed33b513d4ef6eca
Artis Davis <artis.motokross@gmail.com>
2019-05-12 20:58:13 +0300

    got length of a word

---
+++ length.py
@@ -0,0 +1,12 @@
+def length_of_word():
+    correct = False
+    while correct != True:
+        length = input('how long is your word? form 3 to 9:')
+
+        if int(length) < 3 or int(length) > 9:
+            print('sorry you entered worng number ' + length + 'is not between 3 and 9')
+        else:
+            correct = True
+
+
+    return length

db0c17e084ae280de15a99e205425fbf64127bee
Artis Davis <artis.motokross@gmail.com>
2019-05-13 19:13:21 +0300

    guessing letter check if it isn't banned already

--- The_Game.py
+++ The_Game.py
@@ -9,7 +9,29 @@ with open('words.txt', 'r') as f:
 
 for i in maywords:
     if len(i) == ans_word_len:
-        list.append(maywords1, i)
+        g = i.lower()
+        list.append(maywords1, g)
 maywords = maywords1
 maywords1 = ''
+
+#now i have only length of word
+ban = []
+
+print(maywords)
+
+finding_letter = True
+
+while finding_letter:
+    for i in maywords:
+        j = 0
+        while j < ans_word_len:
+            letter = i[j]
+
+            if letter in ban:
+                #then letter is already told
+            else:
+                #then letter is free to use
+
+
+            j += 1
 

9aa35736bc68c2d93c2a04d8cebba240bac095ef
arits04 <artis.davis@outlook.lv>
2019-05-14 15:32:54 +0300

    showing place of letter

--- The_Game.py
+++ The_Game.py
@@ -16,6 +16,29 @@ maywords = maywords1
 maywords1 = ''
 
 #now i have only length of word
+w1 = '_'
+w2 = '_'
+w3 = '_'
+w4 = ''
+w5 = ''
+w6 = ''
+w7 = ''
+w8 = ''
+w9 = ''
+
+#class gettingW:
+if ans_word_len >= 4:
+    w4 = '_'
+if ans_word_len >= 5:
+    w5 = '_'
+if ans_word_len >= 6:
+    w6 = '_'
+if ans_word_len >= 7:
+    w7 = '_'
+if ans_word_len >= 8:
+    w8 = '_'
+if ans_word_len == 9:
+    w9 = '_'
 
 print(maywords)
 
@@ -38,6 +61,28 @@ while finding_letter:
                     if choise == 'y':
                         repeat = False
                         where = input('in which place is letter ' + letter + '?')
+
+                        #class right_letter:
+
+                        if int(where) == 1 and w1 == '_':
+                            w1 = letter
+                        elif int(where) == 2 and w2 == '_':
+                            w2 = letter
+                        elif int(where) == 3 and w3 == '_':
+                            w3 = letter
+                        elif int(where) == 4 and w4 == '_':
+                            w4 = letter
+                        elif int(where) == 5 and w5 == '_':
+                            w5 = letter
+                        elif int(where) == 6 and w6 == '_':
+                            w6 = letter
+                        elif int(where) == 7 and w7 == '_':
+                            w7 = letter
+                        elif int(where) == 8 and w8 == '_':
+                            w8 = letter
+                        elif int(where) == 9 and w9 == '_':
+                            w9 = letter
+                        print(w1 + ' ' + w2 + ' ' + w3 + ' ' + w4 + ' ' + w5 + ' ' + w6 + ' ' + w7 + ' ' + w8 + ' ' + w9)
                         #if word have that letter
 
                     elif choise == 'n':
